8d406ce0451b493887b50924edacfb9040843059
kll <123132>
2020-07-28 09:34:59 +0800

    提交文件

---
+++ helloGit/src/main/java/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * @Author kll
+ * @create 2020-07-28-9:29
+ * @Description
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
